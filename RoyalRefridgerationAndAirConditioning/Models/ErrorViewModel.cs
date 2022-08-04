543bdb445ee8b0358d7faa139b7146d1677d10eb
Farirai <fmadicha@gmail.com>
2022-08-04 16:06:52 +0200

    Add project files.

---
+++ RoyalRefridgerationAndAirConditioning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoyalRefridgerationAndAirConditioning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
